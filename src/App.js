15ccee6b9acc4a4131c09eea33b5ee07014194a3
Asanka Iddamalgoda <asankaasa@gmail.com>
2021-12-15 13:33:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      string : 'Hello'
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {this.state.string}
+        </a>
+
+
+      <button onClick={() => this.setState({string : 'test'})}>click me</button>
+      </header>
+
+    </div>
+    )
+  }
+}
+
+export default App;
